2ffca679fc1e295f080ec16fd5a24377ada5cad6
Juan Castro Fernández <juan.castro@edreamsodigeo.com>
2020-02-18 12:34:58 +0100

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# hangman
+# Hangman
+
+* React
+* Redux
+* Webpack
+* Eslint
+* Sass
+* Babel
+* Jest
+* Enzyme
+
+---
+
+1. Install
+```shell
+npm install
+```
+
+2. Start
+```shell
+npm start
+```
+
+3. Build
+```shell
+npm run build
+```
+
+4. Run tests
+```shell
+npm test
+```
---
+++ src/js/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <h1 className="title">
+                React+Redux hangman
+                </h1>
+            </React.Fragment>
+        );
+    }
+}
