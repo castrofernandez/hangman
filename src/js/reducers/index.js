f8d59b068e9c57d89c9606918cafa18ddde729b7
Juan Castro Fernández <juan.castro@edreamsodigeo.com>
2020-03-13 11:23:49 +0100

    Removes some reducers.

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -3,16 +3,12 @@ import thunk from 'redux-thunk';
 
 import QueryString from '../utils/querystring';
 import languageFilter from './language';
-import remainingLettersFilter from './remaining.letters';
-import wordFilter from './word';
 
 QueryString.load();
 
 const reducers = (state = {}, action) => {
     return {
-        language: languageFilter(state.language, action),
-        remaining: remainingLettersFilter(state.remaining, action),
-        word: wordFilter(state.word, action)
+        language: languageFilter(state.language, action)
     };
 };
 
